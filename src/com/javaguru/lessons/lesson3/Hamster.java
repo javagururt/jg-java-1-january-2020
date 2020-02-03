36fa5d0a69cbeedb1d56c4a28cac8b26bec4676f
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2020-02-03 21:09:38 +0200

    #lesson3 Hamster

---
+++ src/com/javaguru/lessons/lesson3/Hamster.java
@@ -0,0 +1,55 @@
+package com.javaguru.lessons.lesson3;
+
+class Hamster {
+
+    private double weight;
+    private String breed;
+    private String color;
+    private String name;
+    private int age;
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    void eat(double foodWeight) {
+        double newWeight = weight + foodWeight;
+        weight = newWeight;
+    }
+}
---
+++ src/com/javaguru/lessons/lesson3/HamsterTest.java
@@ -0,0 +1,28 @@
+package com.javaguru.lessons.lesson3;
+
+import java.math.BigDecimal;
+
+class HamsterTest {
+
+    public static void main(String[] args) {
+        Hamster firstHamster = new Hamster();
+        firstHamster.setName("Vasya");
+        firstHamster.setAge(1);
+        firstHamster.setColor("Brown");
+        firstHamster.setBreed("Common Hamster");
+        firstHamster.setWeight(100);
+
+        System.out.println("Hamster weight = " + firstHamster.getWeight());
+
+        firstHamster.eat(20.5);
+        System.out.println("Hamster weight = " + firstHamster.getWeight());
+
+        firstHamster.eat(30);
+        firstHamster.eat(30);
+        System.out.println("Hamster weight = " + firstHamster.getWeight() + " kg");
+        System.out.println("Hamster name = " + firstHamster.getName());
+        System.out.println("Hamster color = " + firstHamster.getColor());
+        System.out.println("Hamster breed = " + firstHamster.getBreed());
+        System.out.println("Hamster age = " + firstHamster.getAge());
+    }
+}
